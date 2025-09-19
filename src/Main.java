b3f77ecbdbec196c7695ed8d9ace9539a2df1292
shalasla <layla.shalash1@gmail.com>
2025-09-19 13:02:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2; // You can change this to test other values
+
+        if (numToExamine % 2 == 0) {
+            System.out.println("The number " + numToExamine + " is even.");
+        } else {
+            System.out.println("The number " + numToExamine + " is odd.");
+        }
+    }
+}
